e7534b36ec0f1f661cc64bf0351790591ff055b9
AndyNoob <comfortableandy9082@gmail.com>
2024-07-02 13:01:02 -0700

    Set ChatListener#onChat to LOWEST priority

--- src/main/java/me/comfortable_andy/discordstuff/listener/ChatListener.java
+++ src/main/java/me/comfortable_andy/discordstuff/listener/ChatListener.java
@@ -2,11 +2,13 @@ package me.comfortable_andy.discordstuff.listener;
 
 import me.comfortable_andy.discordstuff.markdown.Markdown;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener {
-    @EventHandler
+
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onChat(AsyncPlayerChatEvent event) {
         event.setMessage(Markdown.convert(event.getMessage()));
     }
